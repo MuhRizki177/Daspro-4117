8d514cbbf62a7fa920335a6558637df8d57768e1
Rizki170 <112882007+Rizki170@users.noreply.github.com>
2022-09-05 22:19:29 +0700

    Create main.cpp

---
+++ Tugas1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Muhammad Rizki" << endl;
+    cout << "A11.2022.14657" << endl;
+    cout << "SMAN 2 Pemalang" << endl;
+    return 0;
+}
