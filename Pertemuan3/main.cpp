74b00323ce25eabd26c8eb418b62687a35da9a50
Muhammad Rizki <112882007+MuhRizki177@users.noreply.github.com>
2022-09-17 21:08:49 +0700

    Add files via upload

---
+++ Pertemuan3/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello!" << endl;
+
+    string nama;
+    cout << "Nama Kamu: ";
+    cin >> nama;
+
+    string nim;
+    cout << "NIM: ";
+    cin >> nim;
+
+    cout << "Hi " << nama;
+    cout << " NIM " << nim;
+    cout << " Selamat Datang! \n" << endl;
+
+    cout << "Aku Muhammad Rizki \nNIM A11.2022.14657" << endl;
+
+    int u=18;
+    float t=160;
+    char b='L';
+    float n=14657;
+    int k=3;
+    float kn=1.5;
+
+    cout << "\nUmur: " <<u;
+    cout << "\nTinggi: " <<t;
+    cout << "\nUkuran Baju: " <<b;
+    cout << "\nMinus Mata Kiri: " <<k;
+    cout << "\nMinus Mata Kanan: " <<kn;
+    return 0;
+}
