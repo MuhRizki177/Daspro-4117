35eb12f806134a013fce8fbce1ec71fa1475aaff
Muhammad Rizki <112882007+MuhRizki177@users.noreply.github.com>
2022-09-25 19:33:34 +0700

    Add files via upload

---
+++ pertemuan5/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+//Nama; Muhammad Rizki
+//NIM: A11.2022.14657
+    float phi=3.14, L;
+    int r,n;
+    cout << "Selamat Datang" << endl;
+    cout << "Ini adalah program Menghitung Luas Lingkaran";
+    cout << "\nMasukkan jari-jari: ";
+    cin >> r;
+    L=phi*r*r;
+    cout << "Luas Lingkaran: " << L;
+
+
+
+    return 0;
+}
